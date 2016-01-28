85d77854f94f6313a5a76076d9032ecbd2580dee
John R. Pelt <johnrpelt@gmail.com>
2016-01-28 10:20:57 -0600

    Updated to babel 6 format

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const qs = require('querystring');
+const webpack = require('webpack');
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+module.exports = {
+  devtool: '#eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/app.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js'],
+    alias: {
+      request: 'browser-request'
+    }
+  },
+  module: {
+    loaders: [
+      // Javascript
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: path.join(__dirname, 'client'),
+        query: {
+          "env": {
+            "development": {
+              "presets": ["react-hmre"],
+              "plugins": [
+                ["react-transform", {
+                  "transforms": [{
+                    "transform": "react-transform-hmr",
+                    "imports": ["react"],
+                    "locals": ["module"]
+                  }]
+                }]
+              ]
+            }
+          },
+        }
+      },
+
+      // CSS
+      {
+        test: /\.css$/,
+        include: path.join(__dirname, 'client'),
+        loader: 'style-loader!css-loader?' + qs.stringify({
+          modules: true,
+          importLoaders: 1,
+          localIdentName: '[path][name]-[local]'
+        })
+      }
+
+    ]
+  }
+};
