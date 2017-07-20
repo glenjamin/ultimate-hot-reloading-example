029c8a7b6cce76e8df4b6900ad04bec608a0a56b
Mantas T <mojavosas@gmail.com>
2017-07-20 19:20:03 +0300

    Add -loader suffix to babel

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ export default {
       // Javascript
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         include: path.join(__dirname, 'client'),
         query: {
           "env": {
