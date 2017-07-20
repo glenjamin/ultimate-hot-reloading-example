636345b333443ae38fe9e771bafcfa17ced1a5ac
Mantas T <mojavosas@gmail.com>
2017-07-20 19:21:15 +0300

    Replace deprecated ErrorsPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ export default {
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   resolve: {
     extensions: ['.js'],
